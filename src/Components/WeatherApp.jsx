99a393499975fdf288e6ae6b89b8a7515004059d
AdilAhmedShekhani <126003246+AdilAhmedShekhani@users.noreply.github.com>
2024-02-16 17:06:41 +0500

    UI Completed

---
+++ src/Components/WeatherApp.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import "./WeatherApp.css";
+
+import search_icon from "../assets/search.png";
+import clear_icon from "../assets/clear.png";
+import cloud_icon from "../assets/cloud.png";
+import drizzle_icon from "../assets/drizzle.png";
+import rain_icon from "../assets/rain.png";
+import snow_icon from "../assets/snow.png";
+import wind_icon from "../assets/wind.png";
+import humidity_icon from "../assets/humidity.png";
+
+const WeatherApp = () => {
+  let api_key = "cdb22b689f4e415d99d2c2a03d95009b";
+  const search = () => {
+    const element = document.getElementsByClassName("cityInput");
+    if (element[0].value === "") {
+      return 0;
+    }
+  };
+  return (
+    <div className="container">
+      <div className="top-bar">
+        <input type="text" className="cityInput" placeholder="Search" />
+        <div
+          className="search-icon"
+          onClick={() => {
+            search();
+          }}
+        >
+          <img src={search_icon} alt="" />
+        </div>
+      </div>
+      <div className="weather-image">
+        <img src={cloud_icon} alt="" />
+      </div>
+      <div className="weather-temp">24°C</div>
+      <div className="weather-location">London</div>
+      <div className="data-container">
+        <div className="element">
+          <img src={humidity_icon} alt="" className="icon" />
+          <div className="data">
+            <div className="humidity-percent">64%</div>
+            <div className="Humidity">Humidity</div>
+          </div>
+        </div>
+        <div className="element">
+          <img src={wind_icon} alt="" className="icon" />
+          <div className="data">
+            <div className="humidity-percent">18 km/h</div>
+            <div className="Humidity">Wind Speed</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WeatherApp;
