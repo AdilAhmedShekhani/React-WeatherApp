56dd488a3bdf1b49a79bbc427a390154c773cbcc
AdilAhmedShekhani <adilaziz2009@live.com>
2024-02-16 19:45:58 +0500

    API Integrated

--- src/Components/WeatherApp.jsx
+++ src/Components/WeatherApp.jsx
@@ -12,11 +12,24 @@ import humidity_icon from "../assets/humidity.png";
 
 const WeatherApp = () => {
   let api_key = "cdb22b689f4e415d99d2c2a03d95009b";
-  const search = () => {
+  const search = async () => {
     const element = document.getElementsByClassName("cityInput");
     if (element[0].value === "") {
       return 0;
     }
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${api_key}`;
+
+    let response = await fetch(url);
+    let data = await response.json();
+    const humidity = document.getElementsByClassName("humidity-percent");
+    const wind = document.getElementsByClassName("wind-rate");
+    const temprature = document.getElementsByClassName("weather-temp");
+    const location = document.getElementsByClassName("weather-location");
+
+    humidity[0].innerHTML = data.main.humidity;
+    wind[0].innerHTML = data.wind.speed;
+    temprature[0].innerHTML = data.main.temp;
+    location[0].innerHTML = data.name;
   };
   return (
     <div className="container">
@@ -41,14 +54,14 @@ const WeatherApp = () => {
           <img src={humidity_icon} alt="" className="icon" />
           <div className="data">
             <div className="humidity-percent">64%</div>
-            <div className="Humidity">Humidity</div>
+            <div className="text">Humidity</div>
           </div>
         </div>
         <div className="element">
           <img src={wind_icon} alt="" className="icon" />
           <div className="data">
-            <div className="humidity-percent">18 km/h</div>
-            <div className="Humidity">Wind Speed</div>
+            <div className="wind-rate">18 km/h</div>
+            <div className="text">Wind Speed</div>
           </div>
         </div>
       </div>
